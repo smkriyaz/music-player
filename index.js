8e0769f0b05a18a1449b1548c0a59a6468b3061b
Your Name <you@example.com>
2023-01-24 17:41:27 +0530

    commied

--- index.js
+++ index.js
@@ -1,36 +1,37 @@
 
-    document.querySelectorAll(".master")[0].addEventListener("click" , songMaster);
+    document.querySelectorAll("button")[2].addEventListener("click" , songMaster);
 
 
-
-function songMaster(){
-    var audio = new Audio('songs/Master.mp3');
+ function songMaster(){
+     var audio = new Audio('songs/Master.mp3');
     audio.play();
-    document.querySelectorAll("p")[0].innerHTML="Now Playing  Vaathi Coming";
-}
+    
+     document.querySelectorAll("p")[0].innerHTML="Now Playing  Vaathi Coming";
+    
+ }
 
-document.querySelectorAll(".maanadu")[0].addEventListener("click" , songmaanadu);
+ document.querySelectorAll("button")[3].addEventListener("click" , songmaanadu);
  
-function songmaanadu(){
-    var audio=new Audio('songs/maanadu.mp3');
+ function songmaanadu(){
+     var audio=new Audio('songs/maanadu.mp3');
     audio.play();
-    document.querySelectorAll("p")[0].innerHTML="Now Playing  Maanaadu";
-}
+     document.querySelectorAll("p")[0].innerHTML="Now Playing  Maanaadu";
+ }
 
-document.querySelectorAll(".lovetoday")[0].addEventListener("click", lovetoday);
+ document.querySelectorAll("button")[4].addEventListener("click", lovetoday);
 
-function lovetoday(){
-    var audio=new Audio('songs/love today.mp3');
+ function lovetoday(){
+     var audio=new Audio('songs/love today.mp3');
+     audio.play();
+     document.querySelectorAll("p")[0].innerHTML="Now Playing  Love Today ";
+ }
+
+
+
+ document.querySelectorAll("button")[5].addEventListener("click", vikram);
+
+ function vikram(){
+     var audio=new Audio('songs/vikram.mp3');
     audio.play();
-    document.querySelectorAll("p")[0].innerHTML="Now Playing  Love Today ";
-}
-
-
-
-document.querySelectorAll(".vikram")[0].addEventListener("click", vikram);
-
-function vikram(){
-    var audio=new Audio('songs/vikram.mp3');
-    audio.play();
-    document.querySelectorAll("p")[0].innerHTML="Now Playing Vikram";
-}
+     document.querySelectorAll("p")[0].innerHTML="Now Playing Vikram";
+ }
